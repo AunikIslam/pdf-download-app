8bcd801899d995f046f3777a4f37ecc723f8aafd
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-08-15 20:07:54 +0600

    permission api test

--- tests/unit.test.js
+++ tests/unit.test.js
@@ -13,10 +13,11 @@ jest.mock('../services/session-context-service', () => {
 
 const utilFunctions = require('../utils/util-functions');
 const baseService = require('../services/base-service');
+const endpoints = require('../config/endpoints');
 
 /** prepare url function test **/
 
-describe('utilFunctions', () => {
+describe('prepare Url', () => {
     it('should correctly prepare API URL', () => {
         const endpoint = '/validate-token';
         const baseUrl = 'f-auth';
@@ -25,20 +26,76 @@ describe('utilFunctions', () => {
     });
 });
 
+/** parse params function test **/
+
+describe('parse params', () => {
+    it('should parse params properly', () => {
+        const params = {
+            marketFilter: '[]',
+            distributorId: '0',
+            retailerId: '0',
+            fromDate: '2025-05-01',
+            toDate: '2025-05-10',
+            organizationId: '0',
+            userId: '0',
+            status: 'string'
+        }
+        const result = utilFunctions.parseParams(params);
+        expect(result).toEqual({
+            marketFilter: [],
+            distributorId: 0,
+            retailerId: 0,
+            fromDate: '2025-05-01',
+            toDate: '2025-05-10',
+            organizationId: 0,
+            userId: 0,
+            status: null
+        })
+    });
+})
+
 /** validate token api test **/
 
-describe('baseService', () => {
+describe('validate token', () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
 
     it('should call validate token api with correct url', async () => {
-        axios.get.mockResolvedValue({ data: { valid: true } });
-        const endpoint = '/validate-token';
-        const baseUrl = 'f-auth';
+        axios.get.mockResolvedValue(
+            {
+                data: true
+            });
+        const endpoint = 'api/v1/auth/validate-token';
+        const baseUrl = 'f-auth/';
         const preparedUrl = utilFunctions.prepareApiUrl(endpoint, baseUrl);
         const result = await baseService.validateToken(preparedUrl);
         expect(axios.get).toHaveBeenCalledWith(preparedUrl, expect.any(Object));
-        expect(result.valid).toBe(true);
+        expect(result).toBe(true);
     });
-})
+});
+
+/** permission api test **/
+
+describe('get permission', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should call permission api with correct url', async () => {
+        axios.get.mockResolvedValue(
+            {
+                data: {
+                    data: {
+                        content: true
+                    }
+                }
+            });
+        const endpoint = endpoints.self_authorities;
+        const baseUrl = 'f-auth/';
+        const preparedUrl = utilFunctions.prepareApiUrl(endpoint, baseUrl);
+        const result = await baseService.getPermissionSet(preparedUrl);
+        expect(axios.get).toHaveBeenCalledWith(preparedUrl, expect.any(Object));
+        expect(result).toBe(true);
+    });
+});
