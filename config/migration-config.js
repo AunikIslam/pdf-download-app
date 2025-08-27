f6a7eace2cc83363f4d5154fbf8d959d636333f7
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-08-27 16:06:33 +0600

    Config Object Generation Logic Updated

--- config/migration-config.js
+++ config/migration-config.js
@@ -1,17 +1,19 @@
 const path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 require('dotenv').config({
   path: path.resolve(__dirname, `../environments/.env.${process.env.NODE_ENV}`)
 });
 
-module.exports = {
-  development: {
+const config = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
     "port": process.env.DB_PORT
-  }
+}
+
+module.exports = {
+    [process.env.NODE_ENV]: config
 }
