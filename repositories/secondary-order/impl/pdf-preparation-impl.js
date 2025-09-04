909c491de30a5e7a28a5e124042612a6ef9bbd3e
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-09-04 21:33:10 +0600

    fonts added to style files

--- repositories/secondary-order/impl/pdf-preparation-impl.js
+++ repositories/secondary-order/impl/pdf-preparation-impl.js
@@ -47,13 +47,15 @@ class PdfPreparationImpl {
     static async prepareSecondaryOrderPdfForAfm(topSheetContent, detailContent) {
         const imagePath = path.join(rootDir, 'public', 'logos', 'rtm.png');
         const imagePathForDetails = path.join(rootDir, 'public', 'logos', 'rtm-small.png');
+        const fontPath = path.join(rootDir, 'public', 'css', 'font-styles.css');
         const cssPathForTopSheet = path.join(rootDir, 'public', 'css', 'afm', 'afm-secondary-order-top-sheet.css');
         const cssPathForDetails = path.join(rootDir, 'public', 'css', 'afm', 'afm-secondary-order-details.css');
 
         const base64 = fs.readFileSync(imagePath).toString('base64');
         const base64ForDetails = fs.readFileSync(imagePathForDetails).toString('base64');
-        const stylesForTopSheet = fs.readFileSync(cssPathForTopSheet, 'utf8');
-        const stylesForDetails = fs.readFileSync(cssPathForDetails, 'utf8');
+        const stylesForFonts = fs.readFileSync(fontPath, 'utf8');
+        const stylesForTopSheet = fs.readFileSync(cssPathForTopSheet, 'utf8') + `\n` + stylesForFonts;
+        const stylesForDetails = fs.readFileSync(cssPathForDetails, 'utf8') + `\n` + stylesForFonts;
 
         const orgLogo = `data:image/png;base64,${base64}`;
         const orgLogoForDetails = `data:image/png;base64,${base64ForDetails}`;
