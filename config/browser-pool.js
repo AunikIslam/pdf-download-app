62fcf7ed6b83082cbf2224f3199618c82bcf7b82
Mezbaul Islam Aunik <mezbaul.aunik@gmail.com>
2025-09-06 09:17:20 +0600

    browser launch log added

--- config/browser-pool.js
+++ config/browser-pool.js
@@ -3,9 +3,11 @@ let browser;
 
 exports.launchBrowser = async () => {
     browser = await chromium.launch({ headless: true });
+    console.log(`Launch browser`);
     return browser;
 }
 
 exports.getBrowser = async () => {
+    console.log(`Get previously launched browser`);
     return browser;
 };
